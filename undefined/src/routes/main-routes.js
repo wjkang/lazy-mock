189ec9cbd8defd22fb5c627c88f33f6a8bfdd5ac
wjkang <977865769@qq.com>
2018-06-28 18:04:01 +0800

    update

--- undefined/src/routes/main-routes.js
+++
@@ -1,4 +0,0 @@
-.get('/role/pagedlist',controllers.role.getRolePagedList)
-.del('/role/del',controllers.role.delRole)
-.del('/role/batchdel',controllers.role.delRoles)
-.post('/role/save',controllers.role.saveRole)
