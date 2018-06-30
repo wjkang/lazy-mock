ffd9993283f4db38905be504dc8c34a3ebec6720
wjk <977865769@qq.com>
2018-06-30 12:21:45 +0800

    update

--- codeGenerate/config/model.js
+++ codeGenerate/config/model.js
@@ -1,18 +1,39 @@
+
+var shortid = require('shortid')
+var Mock = require('mockjs')
+var Random = Mock.Random
+
 export default {
-    name:"book",
-    Name:"Book",
-    properties:[
+    name: "book",
+    Name: "Book",
+    properties: [
         {
-            key:"id",
-            title:"id"
+            key: "id",
+            title: "id"
         },
         {
-            key:"name",
-            title:"书名"
+            key: "name",
+            title: "书名"
         },
         {
-            key:"press",
-            title:"出版社"
+            key: "author",
+            title: "作者"
+        },
+        {
+            key: "press",
+            title: "出版社"
         }
-    ]
+    ],
+    buildMockData: function () {//不需要生成设为false
+        let data = []
+        for (let i = 0; i < 100; 9++) {
+            data.push({
+                id: shortid.generate(),
+                name: Random.cword(5, 7),
+                author: Random.cname(),
+                press: Random.cword(5, 7)
+            })
+        }
+        return data
+    }
 }
