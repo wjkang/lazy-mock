7b305aee7f66283a5390c889f959096d11c3cd9b
wjkang <977865769@qq.com>
2018-09-15 17:00:29 +0800

    udpate

--- src/im/closeMiddleware.js
+++ src/im/closeMiddleware.js
@@ -1,11 +1,14 @@
 export default () => {
     return function (context, next) {
+        let server = context.server;
+        let client = context.client;
         let code = context.code;
         let reason = context.message;
         if (code === 1003 && (reason === 'invalid clientId' || reason === 'exists clientId')) {
             console.log("'invalid clientId' or 'exists clientId' closed")
         } else {
-            console.log(this.clientId + " closed");
+            console.log(server.clients.delete(client.clientId))
+            console.log(client.clientId + " closed");
         }
         next();
     }
