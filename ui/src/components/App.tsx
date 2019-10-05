41cd106dd83208494ebb0352b583c34ddd563764
wjk <977865769@qq.com>
2019-10-06 01:25:08 +0800

    add ui

---
+++ ui/src/components/App.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+
+const reactLogo = require("./../assets/img/react_logo.svg");
+import "./../assets/scss/App.scss";
+
+class App extends React.Component<{}, undefined> {
+    public render() {
+        return (
+            <div className="app">
+                <h1>Hello World!</h1>
+                <p>Foo to the barz</p>
+                <img src={reactLogo} height="480"/>
+            </div>
+        );
+    }
+}
+
+declare let module: object;
+
+export default hot(module)(App);
