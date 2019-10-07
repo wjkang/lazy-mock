5d7d6dea2d9d281487d91038c1e6e31d27cbcd28
wjk <977865769@qq.com>
2019-10-08 02:00:49 +0800

    update

--- ui/nest-server/app.controller.ts
+++ ui/nest-server/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import * as execa from 'execa'
+import * as path from 'path'
+import * as fs from 'fs'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello()
+	}
+
+	@Get('run')
+	run(): string {
+		const subprocess = execa('npm', ['run', 'start'], {
+			stdio: ['inherit', 'pipe', 'inherit'],
+			cwd: path.join('G:\\GitHubProject\\lazy-mock')
+		})
+		subprocess.stdout.on('data', buffer => {
+			const text = buffer.toString().trim()
+			if (text) {
+				console.log(123,text)
+			}
+		})
+		return '1111'
+	}
 }
